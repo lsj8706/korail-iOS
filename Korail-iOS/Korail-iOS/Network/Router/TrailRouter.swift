fb347568fe6001686757fb9070b5d36c034d1303
Sejin Lee <lsj8706@naver.com>
2022-11-21 00:31:39 +0900

    [Feat] #11 - TrailRouter와 ResponseDto 생성

--- Korail-iOS/Korail-iOS/Network/Router/TrailRouter.swift
+++ Korail-iOS/Korail-iOS/Network/Router/TrailRouter.swift
@@ -6,3 +6,39 @@
 //
 
 import Foundation
+import Moya
+
+enum TrailRouter {
+    case getTrailLocation
+}
+
+extension TrailRouter: TargetType {
+    var baseURL: URL {
+        return URL(string: APIConstants.baseURL)!
+    }
+    
+    var path: String {
+        switch self {
+        case .getTrailLocation:
+            return "/trail"
+        }
+    }
+    
+    var method: Moya.Method {
+        switch self {
+        case .getTrailLocation:
+            return .get
+        }
+    }
+    
+    var task: Moya.Task {
+        switch self {
+        case .getTrailLocation:
+            return .requestPlain
+        }
+    }
+    
+    var headers: [String : String]? {
+        return APIConstants.baseHeader
+    }
+}
