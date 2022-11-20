2bb79c2f40283287c61a017aa47b8b90a92dfd9d
Sejin Lee <lsj8706@naver.com>
2022-11-21 00:49:15 +0900

    [Feat] #11 - LocationFormVC 서버 통신 데이터 바인딩

--- Korail-iOS/Korail-iOS/Network/Foundation/APIConstants.swift
+++ Korail-iOS/Korail-iOS/Network/Foundation/APIConstants.swift
@@ -8,6 +8,6 @@
 import Foundation
 
 struct APIConstants {
-    static let baseURL = "http://43.200.249.186:3000/trail"
+    static let baseURL = "http://43.200.249.186:3000"
     static let baseHeader = ["Content-Type": "application/json"]
 }
