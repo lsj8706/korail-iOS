c66fffd9fdba3f32823286c014314cf471b09b86
dlwogus0128 <79050615+dlwogus0128@users.noreply.github.com>
2022-11-25 13:11:26 +0900

    [feat] #10-TrainInquiryResponseDto 작성

---
+++ Korail-iOS/Korail-iOS/Network/Dto/TrainInquiryResponseDto.swift
@@ -0,0 +1,36 @@
+//
+//  TrainInquiryResponseDto.swift
+//  Korail-iOS
+//
+//  Created by 몽이 누나 on 2022/11/25.
+//
+
+import Foundation
+
+struct TrailInquiryResponseDto: Codable {
+    let status: Int
+    let message: String
+    let data: [TrailInquiryResponseData]
+}
+
+struct TrailInquiryResponseData: Codable {
+    let id: Int
+    let departures, arrivals, startDate, trainName, startTime, lastTime: String
+    let trainInformation: Array<Any>
+    let normalPrice: Int
+    let specialPrice: Int
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case departures = "Departures"
+        case arrivals = "Arrivals"
+        case startDate = "StartDate"
+        case trainName = "trainName"
+        case startTime = "StartTime"
+        case lastTime = "LastTime"
+        case trainInformation = "TrainInformation"
+        case normalPrice = "NormalPrice"
+        case specialPrice = "SpecialPrice"
+        
+    }
+}
